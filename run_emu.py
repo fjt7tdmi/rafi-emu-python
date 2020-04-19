b5376f92326f74f8a330fa457733b039dba73036
Akifumi Fujita <fujita.akifumi@gmail.com>
2020-04-19 13:17:29 +0900

    add run_emu.py 1st version

---
+++ run_emu.py
@@ -0,0 +1,29 @@
+# Copyright 2018 Akifumi Fujita
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+
+DefaultCycle = 100
+
+def RunEmulation(args):
+    print(args.file)
+    print(args.cycle)
+
+parser = argparse.ArgumentParser(description="Toy RISCV emulator by Python.")
+parser.add_argument('file', help="Binary file to load")
+parser.add_argument('-c', '--cycle', default=DefaultCycle, help="Number of emulation cycles.")
+
+args = parser.parse_args()
+
+RunEmulation(args)
